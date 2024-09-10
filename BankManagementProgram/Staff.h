466c4752752b3e9020e71c9978dd50c26b4f82c1
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-10 00:21:39 -0700

    Update Staff.h

--- BankManagementProgram/Staff.h
+++ BankManagementProgram/Staff.h
@@ -6,4 +6,20 @@ void staff()
 	BSTTree t;
 	HashTable h;
 	int condition = 0;
+
+	while (condition != 5)
+	{
+		cout << "welcome STAFF" << endl;
+		cout << "choose the following please" << endl;
+		cout << "press 1 to see transaction history  " << endl;
+		cout << "press 2 to transfer " << endl;
+		cout << "press 3 to withdraw " << endl;
+		cout << "press 4 to deposit " << endl;
+		cout << "press 5 to exit" << endl;
+		cin >> condition;
+		if (condition == 1)
+		{
+			// transaction file print karani ha account search kr k
+		}
+	}
 }
